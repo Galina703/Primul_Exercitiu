d6a9f87cd7e556bd0e3c5c23fee41bd6de3ad701
Neo-PC <galinabunu26@gmail.com>
2026-05-12 18:50:10 +0200

    Change number port

--- src/main/java/CountryController.java
+++ src/main/java/CountryController.java
@@ -6,14 +6,17 @@ public class CountryController {
 
     @GetMapping("/moldova")
     public String moldova() {
+
         return "Capitala este Chisinau";
     }
     @GetMapping("/france")
     public String france() {
+
         return "Capitala este Paris";
     }
     @GetMapping("/germani")
     public String germani() {
+
         return "Capitala este Berlin";
     }
 }
