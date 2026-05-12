9a67ebd60bbe09b72176c70f5320c85d281ed332
Neo-PC <galinabunu26@gmail.com>
2026-05-12 15:34:50 +0200

    Revert "Add countryController" This reverts commit b64f1dce5d4670c4f7218745fd704da70a784847.

--- src/main/java/CountryController.java
+++ src/main/java/CountryController.java
@@ -1,25 +1,2 @@
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-   public class CountryController {
-
-    @GetMapping("/moldova")
-    public String getMoldova() {
-        return "Capitale este Chisinau" ;
-    }
-
-        @GetMapping("/france")
-    public String getFrance() {
-            return "Capitale este Paris";
-        }
-
-        @GetMapping("/germany")
-    public String getGermany() {
-                return "Capitale este Berlin";
-            }
-
-
-
-    }
-
+public class CountryController {
+}
