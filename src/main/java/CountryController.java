424e4ce23dd3f508947782ec395296eca9cc653e
Neo-PC <galinabunu26@gmail.com>
2026-05-12 15:52:10 +0200

    Add CountryController

--- src/main/java/CountryController.java
+++ src/main/java/CountryController.java
@@ -1,2 +1,19 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CountryController {
+
+    @GetMapping("/moldova")
+    public String moldova() {
+        return "Capitala este Chisinau";
+    }
+    @GetMapping("/france")
+    public String france() {
+        return "Capitala este Paris";
+    }
+    @GetMapping("/germani")
+    public String germani() {
+        return "Capitala este Berlin";
+    }
 }
